ec879558052f9957931361d556015a2578bf3399
Hridhyapaul <hpdesign2021@gmail.com>
2023-04-05 02:01:15 +0600

    React router

---
+++ src/Components/FriendDetails/FriendDetails.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './FriendDetails.css'
+import { useLoaderData } from 'react-router-dom';
+
+const FriendDetails = () => {
+    const friend = useLoaderData();
+    console.log(friend)
+    return (
+        <div className='details'>
+            <p>Username: {friend.username}</p>
+            <p>Email: {friend.email}</p>
+            <p>Phone: {friend.phone}</p>
+            <p>Website: {friend.website}</p>
+        </div>
+    );
+};
+
+export default FriendDetails;
